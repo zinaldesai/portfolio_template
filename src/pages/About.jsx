6ef1f38dc15d9cbd0ac7159a3d6740f18987ac7c
Zinal Desai <desaizinal15@gmail.com>
2025-03-29 00:29:44 -0500

    about info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,24 @@
-import { NavLink } from 'react-router-dom';
-import PlaceholderImage from '../assets/placeholder_image.png';
+import Image from '../assets/pic.jpg';
 
-// Replace these variables with your information
 const firstName = 'Zinal';
-const jobTitle = 'Replace with your title';
-const company = 'Replace with your company name';
-const mainDuty = 'Replace with your main duty';
+const jobTitle = 'software developer';
 const jobLocation = 'Dallas, TX';
-const briefJobDescription = 'Replace with a brief description of your job';
-const careerObjective = 'Replace with your career objective';
-const personalLife = 'Replace with a brief description of your personal life';
+const careerObjective = 'I have a passion for building innovative and impactful technology solutions. Currently, I\'ve immersed myself as a Full Stack Developer trainee in NPower\'s Path2Tech Program to further my skills in both front and back end development. Ultimately, I aspire to work in a dynamic environment where I can collaborate with like-minded professionals and make a positive impact through the development and application of AI technologies.'
+const goals = 'With a bachelor\'s degree in Cognitive Science with a concentration in Psychology/Human-Computer Interactions,  I am dedicated to bridging the gap between human behavior and technology. Through ongoing training and hands-on projects, I am continually expanding my expertise in the tech field. My passion stems from a deep-rooted desire to not only advance technology but to be part of a larger movement—creating innovative solutions that have a lasting, positive impact on society.';
+const freeTime = 'During my free time, I enjoy baking, cooking, spending time with my friends and family, and playing pickleball.';
 
 const About = () => {
   return (
     <main className="about-container">
       <section className='about-content-container'>
         <section className='image-container'>
-          {/* Replace this placeholder image and alt tag with a professional headshot */}
-          <img className='headshot' src={PlaceholderImage} alt="placeholder image" />
+          <img className='grad-pic' src={Image} alt="graduation picture" />
         </section>
         <section className='description-container'>
-          <h2 className='text-xl font-medium'>I&apos;m {firstName}, a {jobTitle} working for, {company} on {mainDuty} located in, {jobLocation}</h2>
-          <p>{briefJobDescription}</p>
+          <h2 className='text-xl font-medium'>Hi there! I'm {firstName}, a {jobTitle} from {jobLocation}.</h2>
           <p>{careerObjective}</p>
-          <p>{personalLife}</p>
-          <p className='mb-4'>If you are interested in my work, reach out to me!</p>
-          <NavLink to="/contact" className="contact-me-btn">Contact Me</NavLink>
+          <p>{goals}</p>
+          <p>{freeTime}</p>
         </section>
       </section>
     </main>
